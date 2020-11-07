f96a198574f27a4cac12798cc1073086a1db40eb
jshen12815 <jaebli394@gmail.com>
2020-11-06 22:16:19 -0500

    rewire

--- experiment.js
+++ experiment.js
@@ -49,3 +49,26 @@ function xAnime(x) {
       content.style.maxHeight = content.scrollHeight + "px";
     }*/ 
 }
+
+
+function copyDivToClipboard() {
+ var range = document.createRange();
+                   range.selectNode(document.getElementById("emailCopy"));
+                    window.getSelection().removeAllRanges(); // clear current selection
+                    window.getSelection().addRange(range); // to select text
+                    document.execCommand("copy");
+                    window.getSelection().removeAllRanges();// to deselect
+  var tooltip = document.getElementById("myTooltip");
+  var tooltip2 = document.getElementById("myTooltip2");
+
+  tooltip.innerHTML = "🥳 Copied! TTYS! ";
+  tooltip2.innerHTML = "🥳 Copied! TTYS! ";
+                }
+
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  var tooltip2 = document.getElementById("myTooltip2");
+  tooltip.innerHTML = "📋 Click to copy";
+  tooltip2.innerHTML = "📋 Click to copy";
+}
